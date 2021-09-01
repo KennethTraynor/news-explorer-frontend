3337c4c93ac90407b1395daea7a250e10dd905c5
Kenneth Traynor <cyannotive@gmail.com>
2021-09-01 16:50:17 -0400

    feat: create intial navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import './Header.css';
+import Navbar from '../Navbar/Navbar';
 
 function Header() {
     return (
         <header className="header">
-                <div className="navbar"></div>
+            <Navbar />
             <div className="header__container">
                 <h1 className="header__title">What's going on in the world?</h1>
                 <p className="header__caption">Find the latest news on any topic and save them in your personal account.</p>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <footer className='navbar'>
+            <div className='navbar__container'>
+                <p className='navbar__title'>NewsExplorer</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Navbar;
