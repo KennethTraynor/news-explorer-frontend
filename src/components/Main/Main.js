85ed9c658302d306b68d6ef5b3ca44b6596fdff1
Kenneth Traynor <cyannotive@gmail.com>
2021-09-02 15:49:56 -0400

    feat: add nothing found

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,16 +2,18 @@ import './Main.css';
 
 import Header from '../Header/Header';
 import Preloader from '../Preloader/Preloader';
+import NoResults from '../NoResults/NoResults';
 import About from '../About/About';
 import Footer from '../Footer/Footer';
 
 function Main() {
     return (
         <div className='main'>
-            <Header/>
-            <Preloader/>
-            <About/>
-            <Footer/>
+            <Header />
+            <Preloader />
+            <NoResults />
+            <About />
+            <Footer />
         </div>
     )
 }
---
+++ src/components/NoResults/NoResults.js
@@ -0,0 +1,14 @@
+import './NoResults.css';
+
+function NoResults() {
+    return (
+        <section className='no-results'>
+            <i className='no-results__graphic'></i>
+            <p className='no-results__main-text'>Nothing Found</p>
+            <p className='no-results__sub-text'>Sorry, but nothing matched 
+your search terms.</p>
+        </section>
+    )
+}
+
+export default NoResults;
