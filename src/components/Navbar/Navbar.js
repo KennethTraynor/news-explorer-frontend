c1807c4b734fc93730b621d03bb9d232bed1f094
Kenneth Traynor <cyannotive@gmail.com>
2021-09-08 17:24:41 -0400

    feat: add mobile nav menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,22 +2,38 @@ import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 import logoutIcon from '../../images/logout.svg';
 
-function Navbar({ theme, onSigninPopupOpen }) {
+function Navbar({ theme, onSigninPopupOpen, menuOpen = true }) {
     return (
-        <nav className={'navbar ' + (theme ? 'navbar_theme_' + theme : '')}>
-            <div className='navbar__container'>
-                <p className='navbar__title'>NewsExplorer</p>
-                <ul className='navbar__links'>
-                    <li className='navbar__link-item'><NavLink exact to='/' activeClassName='navbar__link_active' className='navbar__link navbar__link_type_home'>Home</NavLink></li>
-                    <li className='navbar__link-item'><NavLink to='/saved-news' activeClassName='navbar__link_active' className='navbar__link navbar__link_type_articles'>Saved articles</NavLink></li>
-                    <li className='navbar__link-item'>
+        <nav className={'navbar' + (theme ? ' navbar_theme_' + theme : '')}>
+            <div className='navbar__bar'>
+                <div className='navbar__container'>
+                    <p className='navbar__title'>NewsExplorer</p>
+                    <ul className='navbar__nav-items'>
+                        <li className='navbar__nav-item'><NavLink exact to='/' activeClassName='navbar__link_active' className='navbar__link navbar__link_type_home'>Home</NavLink></li>
+                        <li className='navbar__nav-item'><NavLink to='/saved-news' activeClassName='navbar__link_active' className='navbar__link navbar__link_type_articles'>Saved articles</NavLink></li>
+                        <li className='navbar__nav-item'>
+                            <button className='navbar__user-state-button navbar__user-state-button_type_signin' onClick={onSigninPopupOpen}>Sign in</button>
+                        </li>
+                        <li className='navbar__nav-item'>
+                            <button className='navbar__user-state-button navbar__user-state-button_type_name'>Username<div src={logoutIcon} className='navbar__logout-icon'></div></button>
+                        </li>
+                    </ul>
+                    <button className={'navbar__menu-button' + (menuOpen ? ' navbar__menu-button_open' : '')}></button>
+                </div>
+            </div>
+            <div className={'navbar__menu' + (menuOpen ? ' navbar__menu_open' : '')}>
+                <div className='navbar__menu-overlay'></div>
+                <ul className='navbar__menu-container'>
+                    <li className='navbar__nav-item'>
+                        <div className='navbar__menu-links'>
+                            <NavLink exact to='/' className='navbar__link navbar__link_type_home'>Home</NavLink>
+                            <NavLink to='/saved-news' className='navbar__link navbar__link_type_articles'>Saved articles</NavLink>
+                        </div>
+                    </li>
+                    <li className='navbar__nav-item'>
                         <button className='navbar__user-state-button navbar__user-state-button_type_signin' onClick={onSigninPopupOpen}>Sign in</button>
                     </li>
-                    <li className='navbar__link-item'>
-                        <button className='navbar__user-state-button navbar__user-state-button_type_name'>Username<div src={logoutIcon} className='navbar__logout-icon'></div></button>
-                    </li>
                 </ul>
-                <button className='navbar__menu-button'></button>
             </div>
         </nav>
     )
